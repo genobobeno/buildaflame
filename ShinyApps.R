c57adf280042012e83125220c95fad10cdd481ef
genobobeno <eugene.quickreaction@gmail.com>
2020-05-11 21:07:35 -0400

    Golem Package created

---
+++ ShinyApps.R
@@ -0,0 +1,9 @@
+rsconnect::setAccountInfo(name='buildaflame',
+                          token='B7307E35E8DD543675C69F3399634BF8',
+                          secret='Y7BBgAFwP1e+Ue7SxaAaiMwzfSyRfJeR2UXrb5qS')
+
+library(rsconnect)
+rsconnect::deployApp('path/to/your/app')
+
+install.packages(c("shinydashboard","golem"),dependencies = TRUE)
+install.packages("devtools",dependencies = T)
